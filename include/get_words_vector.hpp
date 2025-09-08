d29588cec594ce282df3e33f29ac3c4b66ccc92e
sunil <sunil.shastry69@gmail.com>
2025-09-08 00:26:33 -0600

    Create get_words_vector.hpp file with function definition with docs

---
+++ include/get_words_vector.hpp
@@ -0,0 +1,10 @@
+#ifndef __GET_WORDS_VECTOR_HPP__
+#define __GET_WORDS_VECTOR_HPP__
+#include <vector>
+
+/**
+        Retrieve a vector<string> list of all the words from the words.txt file
+*/
+std::vector<std::string> get_words_vector();
+
+#endif
