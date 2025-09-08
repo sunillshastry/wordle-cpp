a194acb1d21b01a7ccf45bc110b1a916d38d1a97
sunil <sunil.shastry69@gmail.com>
2025-09-08 00:39:59 -0600

    Implement the string_trim function in utils/string_trim.cpp file

---
+++ utils/string_trim.cpp
@@ -0,0 +1,22 @@
+#include "./../include/string_trim.hpp"
+
+std::string string_trim(const std::string &str) {
+  size_t first = 0;
+  size_t last = str.size();
+
+  while (first < str.size() &&
+         std::isspace(static_cast<unsigned char>(str[first]))) {
+    first++;
+  }
+
+  if (first == str.size()) {
+    return "";
+  }
+
+  last = str.size() - 1;
+  while (last > first && std::isspace(static_cast<unsigned char>(str[last]))) {
+    last--;
+  }
+
+  return str.substr(first, last - first + 1);
+}
