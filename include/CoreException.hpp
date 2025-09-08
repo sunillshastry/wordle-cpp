280f9d8b4fe4c4abd71ff2ad2dab644cd8388399
sunil <sunil.shastry69@gmail.com>
2025-09-08 00:52:38 -0600

    Define the CoreException class definition with methods and constructors

---
+++ include/CoreException.hpp
@@ -0,0 +1,23 @@
+#ifndef __CORE_EXCEPTION_HPP__
+#define __CORE_EXCEPTION_HPP__
+
+#include <exception>
+#include <string>
+
+class CoreException : public std::exception {
+private:
+  std::string message;
+  int code;
+
+public:
+  CoreException();
+  CoreException(const std::string message);
+  CoreException(const int code);
+
+  CoreException(const std::string message, const int code);
+
+  std::string get_message();
+  int get_code();
+};
+
+#endif
