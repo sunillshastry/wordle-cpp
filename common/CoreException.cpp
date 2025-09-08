bce5ad867a88b4119983c09cad41aca1b9614ba5
sunil <sunil.shastry69@gmail.com>
2025-09-08 00:53:15 -0600

    Develop the CoreException class implementation in CoreException.cpp source file

---
+++ common/CoreException.cpp
@@ -0,0 +1,25 @@
+#include "../include/CoreException.hpp"
+
+CoreException::CoreException() {
+  this->message = "";
+  this->code = 0;
+}
+
+CoreException::CoreException(const std::string message) {
+  this->message = message;
+  this->code = 0;
+}
+
+CoreException::CoreException(const int code) {
+  this->message = "";
+  this->code = code;
+}
+
+CoreException::CoreException(const std::string message, const int code) {
+  this->message = message;
+  this->code = code;
+}
+
+std::string CoreException::get_message() { return this->message; }
+
+int CoreException::get_code() { return this->code; }
